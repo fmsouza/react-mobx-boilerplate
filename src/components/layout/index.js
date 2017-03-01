22d78e76e9ae82029ea2a3874cdb03eef780bafc
Fred Souza <fredericoamsouza@gmail.com>
2017-03-01 15:57:12 -0300

    Adding enxyme and dev tools

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DevTools from 'mobx-react-devtools';
 import { Header } from './fragments';
 
 export const Layout = ({ children }) => (
@@ -10,5 +11,6 @@ export const Layout = ({ children }) => (
     <div className="container">
       {children}
     </div>
+    <DevTools />
   </div>
 );
--- src/components/layout/layout.test.js
+++ src/components/layout/layout.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import { Layout } from './';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Layout />, div); 
+  const wrapper = shallow(<Layout />);
 });
